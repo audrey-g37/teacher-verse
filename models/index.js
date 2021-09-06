9b7592a8d606adf3cfc6e2e924056b7c18f50d35
Audrey <audrey.gillies@gmail.com>
2021-09-06 13:16:58 -0600

    edited student to include id's from other tables

--- models/index.js
+++ models/index.js
@@ -1,20 +1,23 @@
-const Teacher = require('./teacher');
-const Assignment =require('./assignment');
-const Attendance = require('./attendence');
-const Behavior = require('./behavior');
-const Communication =require('./communication');
-const Guardian = require('./guardian');
-const Student = require ('./student');
+const Teacher = require("./teacher");
+const Assignment = require("./assignment");
+const Attendance = require("./attendence");
+const Behavior = require("./behavior");
+const Communication = require("./communication");
+const Guardian = require("./guardian");
+const Student = require("./student");
+const { hasOne } = require("./teacher");
 
+Student.hasOne(Teacher, {
+  foreignKey: "teacher_id",
+  onDelete: "CASCADE",
+});
 
-
-
-
-
-
-
-
-
-
-module.exports = { Teacher, Assignment, Attendance, Behavior, Communication,Guardian, Student }
-
+module.exports = {
+  Teacher,
+  Assignment,
+  Attendance,
+  Behavior,
+  Communication,
+  Guardian,
+  Student,
+};
