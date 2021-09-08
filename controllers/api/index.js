7e5dada6542fea9f41e4b30cb045d5baa78d66ea
Edgar Calderon <edgarcalderon@Edgars-MacBook-Pro.local>
2021-09-08 15:24:28 -0600

    updating json to seed properly and routes for behavior

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const assignmentRoutes = require('./assignment-routes');
-
-
+const assignmentFeedbackRoutes = require('./assignmentFeedback-routes');
+const behaviorRoutes = require ('./behavior-routes');
 
 
 
 
 router.use('/assignment', assignmentRoutes);
-
+router.use('/assignmentFeedback', assignmentFeedbackRoutes)
+router.use('/behavior', behaviorRoutes)
 
 
 
