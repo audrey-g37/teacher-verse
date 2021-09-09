4f0b578bf3850e5e22b914c21e0febeb834421df
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-09-09 15:39:17 -0600

    Configured route for teacher home page and added data to the teacher handlebar

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const loginPage = require("./home-routes");
+const teacherView = require("./user-routes");
 
 router.use("/api", apiRoutes);
 router.use("/", loginPage);
 router.use("/register", loginPage);
+router.use("/teacher", teacherView);
 router.use((req, res) => {
   res.status(403).json({ message: "You dont have access to this page" }); //connecting to handlebars "Catherine"
 });
