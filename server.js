ad72ea36fa497c1f323aa59a3fd8813061e97c3e
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-09 14:05:24 -0600

    updaing code

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const routes = require('./controllers')
-const sequelize = require('./config/connection')
+const express = require("express");
+const exphbrs = require("express-handlebars");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const hbs = exphbs.create({});
 
 app.use(express.json());
-app.use(express.urlencoded({extend: true}));
+app.use(express.urlencoded({ extend: true }));
 app.use(routes);
 
-
-
-sequelize.sync({force: false}).then(() => {
-    app.listen(PORT, () => {
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
     console.log(`app is listening on port ${PORT}`);
-    });
+  });
 });
