8c8efb902a5bf7a75f16b4cb6ac1386d8a347ad5
Audrey <audrey.gillies@gmail.com>
2021-09-09 13:59:41 -0600

    login and register are rendering

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,12 @@
-const router = require ('express').Router();
-const apiRoutes= require('./api')
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const loginPage = require("./home-routes");
 
-router.use('/api',apiRoutes);
-router.use((req,res) => {
- res.status(403).json({message:"You dont have access to this page"}) //connecting to handlebars "Catherine"
+router.use("/api", apiRoutes);
+router.use("/", loginPage);
+router.use("/register", loginPage);
+router.use((req, res) => {
+  res.status(403).json({ message: "You dont have access to this page" }); //connecting to handlebars "Catherine"
 });
-
-
-
 
 module.exports = router;
