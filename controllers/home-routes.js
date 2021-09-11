132c2e13be4a43f60cde09e454ff2184e11b19cb
Audrey <audrey.gillies@gmail.com>
2021-09-10 20:58:57 -0600

    register handlebars is storing new teachers

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,30 @@
 const router = require("express").Router();
+const { Teacher } = require("../models");
 
-router.get("/", async (req, res) => {
+router.get("/login", async (req, res) => {
   res.render("login");
 });
 
 router.get("/register", async (req, res) => {
-  res.render("register");
+  try {
+    res.render("register");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const newTeacher = await Teacher.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json("Register Successful!");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
